6884589d99da1efa8af71e3044c94cf1af9d35cc
NewbergFTC <RevolutionNewbergFTC@gmail.com>
2015-10-21 11:36:20 -0700

    DriveTrain.java
    This is the new coding that works!

---
+++ DriveTrain1.java
@@ -0,0 +1,61 @@
+package us.newberg.bullet;
+
+        import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+        import com.qualcomm.ftcrobotcontroller.FtcRobotControllerActivity;
+        import com.qualcomm.robotcore.hardware.DcMotor;
+        import com.qualcomm.robotcore.hardware.HardwareMap;
+        import com.qualcomm.robotcore.util.Range;
+
+
+/**
+ * Created by btaka on 10/9/2015
+ */
+
+public class DriveTrain extends OpMode{
+    DcMotor leftfront;
+    DcMotor leftback;
+    DcMotor rightfront;
+    DcMotor rightback;
+
+
+    @Override
+    public void init() {
+        leftfront = hardwareMap.dcMotor.get("lf");
+        leftback = hardwareMap.dcMotor.get("lb");
+        rightfront = hardwareMap.dcMotor.get("rf");
+        rightback = hardwareMap.dcMotor.get("rb");
+    }
+
+    @Override
+    public void loop() {
+        float leftY = -gamepad1.left_stick_y;
+        float rightY = -gamepad1.right_stick_y;
+        // float leftX= -gamepad1.left_ stick_x;
+        //float rightX= -gamepad1.right_stick_x;
+        Range.clip(leftY,-1,1);
+        Range.clip(rightY,-1,1);
+        /*
+        {
+            if (gamepad1.a)
+                leftfront.setPower(leftY);
+                rightfront.setPower(rightY);
+                leftback.setPower(leftY);
+                rightback.setPower(rightY);
+        }
+
+        {
+            if (gamepad1.b)
+            leftback.setPower(leftY);
+            rightback.setPower(rightY);
+            leftfront.setPower(0);
+            rightfront.setPower(0);
+        }
+
+            */
+            leftfront.setPower(leftY);
+            rightfront.setPower(rightY);
+            leftback.setPower(leftY);
+            rightback.setPower(rightY);
+
+        }
+    }
