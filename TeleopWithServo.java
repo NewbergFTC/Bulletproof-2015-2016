f9bac864f7430910d2b478d1a2eca9247d968f23
NewbergFTC <RevolutionNewbergFTC@gmail.com>
2015-11-04 09:38:54 -0800

    Create TeleopWithServo.java

---
+++ TeleopWithServo.java
@@ -0,0 +1,52 @@
+package com.qualcomm.ftcrobotcontroller.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+//import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+//import com.qualcomm.ftcrobotcontroller.FtcRobotControllerActivity;
+//import com.qualcomm.robotcore.hardware.HardwareMap;
+//import com.qualcomm.robotcore.util.Hardware;
+import com.qualcomm.robotcore.hardware.Servo;
+//A base servo program to go off of for future use
+public class TeleopWithServoArms extends OpMode {
+    DcMotor leftfront;
+    DcMotor leftback;
+    DcMotor rightfront;
+    DcMotor rightback;
+    Servo leftArm;
+    Servo rightArm;
+    final double LEFT_ARM_OPEN = .5;
+    final double LEFT_ARM_CLOSED = 0.0;
+    final double RIGHT_ARM_OPEN = 0.5;
+    final double RIGHT_ARM_CLOSED = 0.0;
+    @Override
+    public void init(){
+        leftfront = hardwareMap.dcMotor.get("lf");
+        leftback = hardwareMap.dcMotor.get("lb");
+        rightfront = hardwareMap.dcMotor.get("rf");
+        rightback = hardwareMap.dcMotor.get("rb");
+        leftback.setDirection(DcMotor.Direction.REVERSE);
+        leftfront.setDirection(DcMotor.Direction.REVERSE);
+        leftArm = hardwareMap.servo.get("leftArm");
+        rightArm = hardwareMap.servo.get("rightArm");
+        rightArm.setPosition(RIGHT_ARM_CLOSED);
+        leftArm.setPosition(LEFT_ARM_CLOSED);
+    }
+    @Override
+    public void loop() {
+        float leftY = -gamepad1.left_stick_y;
+        float rightY = -gamepad1.right_stick_y;
+        leftfront.setPower(leftY);
+        rightfront.setPower(rightY);
+        leftback.setPower(leftY);
+        rightback.setPower(rightY);
+        if(gamepad1.a){
+            leftArm.setPosition(LEFT_ARM_OPEN);
+            rightArm.setPosition(RIGHT_ARM_OPEN);
+        }
+        if(gamepad1.y){
+            leftArm.setPosition(LEFT_ARM_CLOSED);
+            rightArm.setPosition(RIGHT_ARM_CLOSED);
+        }
+    }
+}
