842aced6c2bc069f081a9645c7f2b7fc380196c7
NewbergFTC <RevolutionNewbergFTC@gmail.com>
2015-11-03 21:14:58 -0800

    Update LightSensorTest.java

--- LightSensorTest.java
+++ LightSensorTest.java
@@ -4,35 +4,35 @@ import com.qualcomm.ftcrobotcontroller.FtcRobotControllerActivity;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.util.Hardware;
-
-public class LightSensorTest extends OpMode{
+import com.qualcomm.robotcore.hardware.OpticalDistanceSensor;
+import com.qualcomm.hardware.ModernRoboticsNxtLightSensor;
+public class lightsensortest extends OpMode {
     DcMotor lf;
     DcMotor lb;
     DcMotor rf;
     DcMotor rb;
-    float lightSensor;
-    
-     public void init(){
+    ModernRoboticsNxtLightSensor lightSensor;
+    public void init() {
         lf = hardwareMap.dcMotor.get("lf");
         lb = hardwareMap.dcMotor.get("lb");
         rf = hardwareMap.dcMotor.get("rf");
         rb = hardwareMap.dcMotor.get("rb");
-        lightSensor = hardwareMap. opticalDistanceSensor.get("lightsensor")
+        //lightSensor = hardwareMap.
     }
     @Override
     public void loop() {
-    float lightsensordata = lightSensor.getLightDetected();
-      if (lightsensordata => 80){
-        lf.setPower(0)
-        lb.setPower(0)
-        rf.setPower(0)
-        rb.setPower(0)
-        }else{
-        lf.setPower(left)
-        lb.setPower(left)
-        rf.setPower(right)
-        rb.setPower(right)
+        Double lightsensordata = lightSensor.getLightDetected();
+        if (lightsensordata > 80) {
+            lf.setPower(0);
+            lb.setPower(0);
+            rf.setPower(0);
+            rb.setPower(0);
+        } else {
+            lf.setPower(1);
+            lb.setPower(1);
+            rf.setPower(1);
+            rb.setPower(1);
         }
-        telemetry.addData("lightsensordata value", lightsensordata)
+        telemetry.addData("lightsensordata value", lightsensordata);
     }
-      
+}
