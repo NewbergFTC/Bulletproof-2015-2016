2b1bbb6e5685381cfaa67484593fe79a46302ff0
NewbergFTC <RevolutionNewbergFTC@gmail.com>
2015-11-04 15:30:48 -0800

    Update SlowDownWheelTeleop.java
    Slow downs the Wheels by powering the wheels for 0.010 seconds and making them sleep for the same amount of time

--- SlowDownWheelTeleop.java
+++ SlowDownWheelTeleop.java
@@ -1,63 +1,63 @@
 package us.newberg.bullet;
 
         import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-        import com.qualcomm.ftcrobotcontroller.FtcRobotControllerActivity;
-        import com.qualcomm.robotcore.hardware.DcMotor;
-        import com.qualcomm.robotcore.hardware.HardwareMap;
-        import com.qualcomm.robotcore.util.Range;
-        import com.qualcomm.robotcore
-        
-        public class Driving extends OpMode( 
+import com.qualcomm.ftcrobotcontroller.FtcRobotControllerActivity;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.Range;
+import com.qualcomm.robotcore
+
+public class Driving extends OpMode()
         DcMotor leftfront;
         DcMotor leftback;
         DcMotor rightfront;
         DcMotor rightback;
         ElapsedTime time;
-      
-        static final double fowardtime=0.010
-        static final double sleeping= 0.010
+
+static final double fowardtime=0.010
+static final double sleeping= 0.010
         int count = 0
-        
-        enum State {fowarddrive,sleeping,done};
-        State state;
-        
-        @Override
-        public void init() {
-            leftfront= hardwareMap.dcmotor.get("lf");
-            rightfront= hardwareMap.dcmotor.get("rf);
-            leftback= hardwareMap.dcmotor.get("lb");
-            rightback= hardwareMap.dcmotor.get("rb");
-            leftY= -gamepad.leftstick.y;
-            rightY= -gamepad.leftstick.y;
-            
-            leftback.direction.setDirection(REVERSE);
-            rightback.direction.setDirection(REVERSE);
+
+enum State {fowarddrive,sleeping,done};
+State state;
+
+@Override
+public void init() {
+        leftfront= hardwareMap.dcmotor.get("lf");
+        rightfront= hardwareMap.dcmotor.get("rf);
+        leftback= hardwareMap.dcmotor.get("lb");
+        rightback= hardwareMap.dcmotor.get("rb");
+        leftY= -gamepad.leftstick.y;
+        rightY= -gamepad.leftstick.y;
+
+        leftback.direction.setDirection(REVERSE);
+        rightback.direction.setDirection(REVERSE);
         }
-        
-        public void loop() {
-            double currentTime= time.time();
-            
-            switch (state) {
-                case fowarddrive:
-                  leftfront.setpower(leftY)
-                  rightfront.setpower(right)
-                break;
-                case sleeping:
-                 if (currentTime> fowardtime) {
-                     state = State.sleeping;
-                 }
-                 time.reset
-                 }
+@Override
+public void loop() {
+        double currentTime= time.time();
+
+        switch (state) {
+        case fowarddrive:
+        leftfront.setpower(leftY)
+        rightfront.setpower(right)
+        break;
+        case sleeping:
+        if (currentTime> fowardtime) {
+        state = State.sleeping;
+        }
+        time.reset
+        }
           /*
                 break;
                 case done:
                 // Code...
                 break;
                 default:
-                
+
                 break;
             }
-            
+
           switch(state) {
               leftfront.setpower(leftY)
               rightfront.setpower(right)
@@ -68,15 +68,18 @@ package us.newberg.bullet;
               }
           break;
           */
-          case(sleeping)
-              leftfront.setpower(0)
-              rightfront.setpower(0)
-            if(currentTime> sleeping) {
-              state= State.fowardtime;
-              }
-              time.reset();
-              }
-          break;
-          
-          leftback.setpower(leftY)
-          rightback.setpower(rightY)
+        case(sleeping)
+        leftfront.setpower(0)
+        rightfront.setpower(0)
+        if(currentTime> sleeping) {
+        state= State.fowardtime;
+        }
+        time.reset();
+        }
+        break;
+        }
+
+        leftback.setpower(leftY)
+        rightback.setpower(rightY)
+
+
