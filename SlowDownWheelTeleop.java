5b800017110227f1cd35e9e84f158143cbfcef8e
NewbergFTC <RevolutionNewbergFTC@gmail.com>
2015-11-06 16:34:57 -0800

    Update SlowDownWheelTeleop.java

--- SlowDownWheelTeleop.java
+++ SlowDownWheelTeleop.java
@@ -1,6 +1,5 @@
-package us.newberg.bullet;
-
-        import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+package us.newberg.opmodes;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.ftcrobotcontroller.FtcRobotControllerActivity;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
@@ -13,6 +12,20 @@ public class Driving extends OpMode()
         DcMotor rightfront;
         DcMotor rightback;
         ElapsedTime time;
+        Servo leftArm;
+        Servo rightArm;
+
+final double LEFT_ARM_OPEN = .5;
+final double LEFT_ARM_CLOSED = 0.0;
+final double RIGHT_ARM_OPEN = 0.5;
+final double RIGHT_ARM_CLOSED = 0.0;
+
+        DcMotor ArmTilt;
+        DcMotor ArmLift;
+final double PowerForward = .5;
+final double PowerBack = -.5;
+final double ArmStop = 0;
+
 
 static final double fowardtime=0.010
 static final double sleeping= 0.010
@@ -32,8 +45,10 @@ public void init() {
 
         leftback.direction.setDirection(REVERSE);
         rightback.direction.setDirection(REVERSE);
+        ArmTilt = hardwareMap.dcMotor.get("ArmTilt");
+        ArmLift = hardwareMap.dcMotor.get("ArmLift");
         }
-@Override
+
 public void loop() {
         double currentTime= time.time();
 
@@ -48,26 +63,8 @@ public void loop() {
         }
         time.reset
         }
-          /*
-                break;
-                case done:
-                // Code...
-                break;
-                default:
+        break;
 
-                break;
-            }
-
-          switch(state) {
-              leftfront.setpower(leftY)
-              rightfront.setpower(right)
-              if (currentTime> fowardtime) {
-              state= State.sleeping;
-              }
-              time.reset();
-              }
-          break;
-          */
         case(sleeping)
         leftfront.setpower(0)
         rightfront.setpower(0)
@@ -78,8 +75,42 @@ public void loop() {
         }
         break;
         }
-
         leftback.setpower(leftY)
         rightback.setpower(rightY)
+        if(gamepad2.x){
+        leftArm.setPosition(LEFT_ARM_OPEN);
+        //rightArm.setPosition(RIGHT_ARM_OPEN);
+        }
+        if(gamepad2.b){
+        //leftArm.setPosition(LEFT_ARM_CLOSED);
+        rightArm.setPosition(RIGHT_ARM_OPEN);
+        }
+        if(gamepad2.y){
+        leftArm.setPosition(LEFT_ARM_CLOSED);
+        rightArm.setPosition(RIGHT_ARM_CLOSED);
+        }
+        if(gamepad2.right_trigger > .01){
+        ArmTilt.setPower(PowerForward);
+        }else{
+        ArmTilt.setPower(ArmStop);
+        }
+        if(gamepad2.left_trigger > .01) {
+        ArmLift.setPower(PowerForward);
+        }else{
+        ArmLift.setPower(ArmStop);
+        }
+        if(gamepad2.right_bumper) {
+        ArmTilt.setPower(PowerBack);
+        }else{
+        ArmTilt.setPower(ArmStop);
+        }
+        if(gamepad2.left_bumper) {
+        ArmLift.setPower(PowerBack);
+        }else {
+        ArmLift.setPower(ArmStop);
+        }
+        }
+
+
 
 
