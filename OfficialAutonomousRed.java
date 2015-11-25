8caf866e9f79eafed104e9fc4b150484d0478fd9
NewbergFTC <RevolutionNewbergFTC@gmail.com>
2015-11-25 13:19:11 -0800

    Update OfficialAutonomousRed.java

--- OfficialAutonomousRed.java
+++ OfficialAutonomousRed.java
@@ -13,12 +13,12 @@ public class OfficialAutonomousRed extends BulletProofOpMode {
         while (opModeIsActive()) {
             goForward(42 , MOTOR_POWER_LEFT , MOTOR_POWER_RIGHT);
             sleep(500);
-            goTurn(30 , LEFT, MOTOR_POWER);
+            goTurn(33 , LEFT, TURN_POWER);
             // goForward(20, MOTOR_POWER);
             sleep(500);
             goForward(40 , MOTOR_POWER_LEFT , MOTOR_POWER_RIGHT);
             sleep(500);
-            goTurn(30 , LEFT, MOTOR_POWER);
+            goTurn(30 , LEFT, TURN_POWER);
             sleep(500);
             goForward(36 , 1 , 1); //accelerate into wall
             sleep(500);
@@ -26,3 +26,4 @@ public class OfficialAutonomousRed extends BulletProofOpMode {
         }
     }
 }
+
