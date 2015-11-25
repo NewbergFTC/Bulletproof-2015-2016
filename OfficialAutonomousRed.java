c40f7c40d342b95d77e03387630c582f0c0b77ae
NewbergFTC <RevolutionNewbergFTC@gmail.com>
2015-11-24 21:29:51 -0800

    Create OfficialAutonomousRed.java

---
+++ OfficialAutonomousRed.java
@@ -0,0 +1,28 @@
+package us.newberg.bullet;
+public class OfficialAutonomousRed extends BulletProofOpMode {
+    @Override
+    public void initialize() {
+    }
+    @Override
+    public void Update() {
+    }
+    @Override
+    public void runOpMode() throws InterruptedException {
+        super.Init();
+        waitForStart();
+        while (opModeIsActive()) {
+            goForward(42 , MOTOR_POWER_LEFT , MOTOR_POWER_RIGHT);
+            sleep(500);
+            goTurn(30 , LEFT, MOTOR_POWER);
+            // goForward(20, MOTOR_POWER);
+            sleep(500);
+            goForward(40 , MOTOR_POWER_LEFT , MOTOR_POWER_RIGHT);
+            sleep(500);
+            goTurn(30 , LEFT, MOTOR_POWER);
+            sleep(500);
+            goForward(36 , 1 , 1); //accelerate into wall
+            sleep(500);
+            break;
+        }
+    }
+}
