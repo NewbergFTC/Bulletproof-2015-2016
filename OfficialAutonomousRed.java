1e7388d33029270864a2c2b9509ab17b50c5f335
NewbergFTC <RevolutionNewbergFTC@gmail.com>
2015-12-06 14:37:30 -0800

    Update OfficialAutonomousRed.java

--- OfficialAutonomousRed.java
+++ OfficialAutonomousRed.java
@@ -1,30 +1,25 @@
-package us.newberg.bullet;
-public class OfficialAutonomousRed extends BulletProofOpMode {
+package com.qualcomm.ftcrobotcontroller.opmodes;
+public class OfficialAutonomousRed extends BulletOpMode{
+    @Override public void initialize(){}
+    @Override public void Update(){}
     @Override
-    public void initialize() {
-    }
-    @Override
-    public void Update() {
-    }
-    @Override
-    public void runOpMode() throws InterruptedException {
+    public void runOpMode() throws InterruptedException{
         super.Init();
         waitForStart();
-        while (opModeIsActive()) {
+        while (opModeIsActive()){
             goForward(42 , MOTOR_POWER_LEFT , MOTOR_POWER_RIGHT);
-            sleep(500);
+            sleep(10000);
             goTurn(33 , LEFT, TURN_POWER);
-            // goForward(20, MOTOR_POWER);
             sleep(500);
             goForward(40 , MOTOR_POWER_LEFT , MOTOR_POWER_RIGHT);
             sleep(500);
             goTurn(30 , LEFT, TURN_POWER);
             sleep(500);
-            goForward(30 , 1 , 1); //accelerate into wall
+            goForward(30 , MOTOR_POWER_LEFT , MOTOR_POWER_RIGHT);
             sleep(500);
-            armRotate(100 , armForward, PowerForward );
-            sleep(500);
-            break; 
+            //armRotate(80 , armForward, ARM_POWER_FORWARD );
+            //sleep(500);
+            break;
         }
     }
 }
