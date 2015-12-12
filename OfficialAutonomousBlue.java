53090a6242e38ff53ad130139bd05af37f6d2ef6
NewbergFTC <RevolutionNewbergFTC@gmail.com>
2015-12-11 20:18:14 -0800

    Update OfficialAutonomousBlue.java

--- OfficialAutonomousBlue.java
+++ OfficialAutonomousBlue.java
@@ -1,23 +1,35 @@
-package com.qualcomm.ftcrobotcontroller.opmodes;
-public class OfficialAutonomousBlue extends BulletOpMode {
-    @Override public void initialize(){}
-    @Override public void Update(){}
+package com.qualcomm.ftcrobotcontroller.us.newberg.bullet;
+public class OfficialAutonomousBlue extends BulletProofMode {
     @Override
-    public void runOpMode() throws InterruptedException{
+    public void initialize() {
+    }
+    @Override
+    public void Update() {
+    }
+    @Override
+    public void runOpMode() throws InterruptedException {
         super.Init();
         waitForStart();
-        while (opModeIsActive()){
-            goForward(42 , MOTOR_POWER_LEFT , MOTOR_POWER_RIGHT);
-            sleep(10000);
-            goTurn(33 , RIGHT, TURN_POWER);
+        while (opModeIsActive()) {
+            JohnCena.start();
+            if (goForward(42 , MOTOR_POWER_LEFT , MOTOR_POWER_RIGHT , 3500))
+                return;
+
             sleep(500);
-            goForward(40 , MOTOR_POWER_LEFT , MOTOR_POWER_RIGHT);
+            goTurn(33 , RIGHT, TURN_POWER);
+            // goForward(20, MOTOR_POWER);
+            sleep(500);
+            if ( goForward(40 , MOTOR_POWER_LEFT , MOTOR_POWER_RIGHT , 3500))
+                return;
+
             sleep(500);
             goTurn(30 , RIGHT, TURN_POWER);
             sleep(500);
-            goForward(30 , MOTOR_POWER_LEFT , MOTOR_POWER_RIGHT);
+            if (goForward(36 , 1 , 1 , 3500)) //accelerate into wall
+                return;
+
             sleep(500);
-            //armRotate(80 , armForward, ARM_POWER_FORWARD );
+            //armRotate(100 , armForward, PowerForward );
             //sleep(500);
             break;
         }
