a9d836c1e31f49b74ba9b16e0ba3fb32556b7bba
NewbergFTC <RevolutionNewbergFTC@gmail.com>
2016-01-11 15:29:11 -0800

    Update OfficialAutonomousBlue.java

--- OfficialAutonomousBlue.java
+++ OfficialAutonomousBlue.java
@@ -1,5 +1,8 @@
-package com.qualcomm.ftcrobotcontroller.us.newberg.bullet;
-public class OfficialAutonomousBlue extends BulletProofMode {
+package com.qualcomm.ftcrobotcontroller.opmodes;
+
+import com.qualcomm.ftcrobotcontroller.opmodes.BulletOpMode;
+
+public class OfficialAutonomousBlue extends BulletOpMode {
     @Override
     public void initialize() {
     }
@@ -11,21 +14,28 @@ public class OfficialAutonomousBlue extends BulletProofMode {
         super.Init();
         waitForStart();
         while (opModeIsActive()) {
-            JohnCena.start();
-            goForward(42 , LEFT_FRONT_POWER ,RIGHT_FRONT_POWER,LEFT_BACK_POWER,RIGHT_BACK_POWER,5000);
-            sleep(10000);
-            goTurn(33 , RIGHT, LEFT_FRONT_POWER,RIGHT_FRONT_POWER,LEFT_BACK_POWER,RIGHT_BACK_POWER);
+            //JohnCena.start();
+            if (goForward(5 , LEFT_FRONT_POWER,RIGHT_FRONT_POWER,LEFT_BACK_POWER ,RIGHT_BACK_POWER,3500 ))
+                return;
+            sleep(9000);
+            if (goForward(37 , LEFT_FRONT_POWER,RIGHT_FRONT_POWER,LEFT_BACK_POWER ,RIGHT_BACK_POWER,3500 ))
+                return;
+            sleep(500);
+            goTurn(64, RIGHT, LEFT_FRONT_POWER,RIGHT_FRONT_POWER,LEFT_BACK_POWER,RIGHT_BACK_POWER);
             // goForward(20, MOTOR_POWER);
             sleep(500);
-            goForward(40 , LEFT_FRONT_POWER , RIGHT_FRONT_POWER,LEFT_BACK_POWER,RIGHT_BACK_POWER,5000);
+            if (goForward(42 , LEFT_FRONT_POWER , RIGHT_FRONT_POWER,LEFT_BACK_POWER,RIGHT_BACK_POWER,3500))
+                return;
             sleep(500);
-            goTurn(30 , RIGHT, LEFT_FRONT_POWER,RIGHT_FRONT_POWER,LEFT_BACK_POWER,RIGHT_BACK_POWER);
+            goTurn(58, RIGHT, LEFT_FRONT_POWER,RIGHT_FRONT_POWER,LEFT_BACK_POWER,RIGHT_BACK_POWER);
             sleep(500);
-            goForward(36 , .9 , .9 , 1 , 1 , 5000); //accelerate into wall
+            if (goForward(45 , LEFT_FRONT_POWER , RIGHT_FRONT_POWER, LEFT_BACK_POWER , RIGHT_BACK_POWER,3500))
+                return;//accelerate into wall
             sleep(500);
             //armRotate(100 , armForward, PowerForward );
             //sleep(500);
             break;
+
         }
     }
 }
