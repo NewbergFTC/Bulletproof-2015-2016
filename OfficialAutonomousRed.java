1558e0a67e66f334d731dacd5bb9c1baca753900
NewbergFTC <RevolutionNewbergFTC@gmail.com>
2016-01-23 10:24:53 -0800

    Update OfficialAutonomousRed.java

--- OfficialAutonomousRed.java
+++ OfficialAutonomousRed.java
@@ -1,6 +1,7 @@
 package com.qualcomm.ftcrobotcontroller.opmodes;
 
 import com.qualcomm.ftcrobotcontroller.opmodes.BulletOpMode;
+import com.qualcomm.robotcore.hardware.UltrasonicSensor;
 
 public class OfficialAutonomousRed extends BulletOpMode {
     @Override
@@ -14,28 +15,39 @@ public class OfficialAutonomousRed extends BulletOpMode {
         super.Init();
         waitForStart();
         while (opModeIsActive()) {
+            SkirtServoR.setPosition(SERVO_SKIRT_DOWNR);
+            SkirtServoL.setPosition(SERVO_SKIRT_DOWNL);
             Rocky.start();
-            if(goForward(5,LEFT_FRONT_POWER,RIGHT_FRONT_POWER,LEFT_BACK_POWER,RIGHT_BACK_POWER,1000))
-                return;
+            //if(goForward(5,LEFT_FRONT_POWER,RIGHT_FRONT_POWER,LEFT_BACK_POWER,RIGHT_BACK_POWER,1000))
+            //    return;
             sleep(9000);
-            if (goForward(37 , LEFT_FRONT_POWER,RIGHT_FRONT_POWER,LEFT_BACK_POWER ,RIGHT_BACK_POWER,3500 ))
+            if (goForward(45 , LEFT_FRONT_POWER,RIGHT_FRONT_POWER,LEFT_BACK_POWER ,RIGHT_BACK_POWER,5000 ))
                 return;
             sleep(500);
             goTurn(64, LEFT, LEFT_FRONT_POWER, RIGHT_FRONT_POWER, LEFT_BACK_POWER, RIGHT_BACK_POWER);
             sleep(500);
-            if (goForward(52 , LEFT_FRONT_POWER , RIGHT_FRONT_POWER,LEFT_BACK_POWER,RIGHT_BACK_POWER,3500))
+            if (goForward(52 , LEFT_FRONT_POWER , RIGHT_FRONT_POWER,LEFT_BACK_POWER,RIGHT_BACK_POWER,5000))
                 return;
             sleep(500);
             goTurn(60, LEFT, LEFT_FRONT_POWER, RIGHT_FRONT_POWER, LEFT_BACK_POWER, RIGHT_BACK_POWER);
             sleep(500);
-            if (goForward(26 , LEFT_FRONT_POWER , RIGHT_FRONT_POWER, LEFT_BACK_POWER , RIGHT_BACK_POWER,3500))
-                return;//accelerate into wall
+            double ultrasonicvalue = ultrasonicSensor.getUltrasonicLevel();
+            while (ultrasonicvalue > 22){
+                ultrasonicvalue = ultrasonicSensor.getUltrasonicLevel();
+                motorDrive(LEFT_FRONT_POWER,RIGHT_FRONT_POWER,LEFT_BACK_POWER,RIGHT_BACK_POWER);
+            }
+            StopDriveMotors();
+            //if (goForward(28 , LEFT_FRONT_POWER , RIGHT_FRONT_POWER, LEFT_BACK_POWER , RIGHT_BACK_POWER,5000));//accelerate into wall
             sleep(500);
             motorArmRotate(.12,1);
             sleep(5500);
             StopArmMotors();
+            sleep(2000);
+            ArmLiftUp();
             sleep(500);
+            ArmLiftStop();
             break;
+
 
         }
     }
