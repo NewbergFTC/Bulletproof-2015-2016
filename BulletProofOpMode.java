cac41c61b27be45329eb330e830d205612e8e6ff
NewbergFTC <RevolutionNewbergFTC@gmail.com>
2016-02-04 16:49:36 -0800

    Update BulletProofOpMode.java

--- BulletProofOpMode.java
+++ BulletProofOpMode.java
@@ -1,8 +1,8 @@
-package com.qualcomm.ftcrobotcontroller.opmodes;
+package com.qualcomm.ftcrobotcontroller.us.newberg.bullet;
 import android.content.Context;
 import android.media.MediaPlayer;
 import android.net.Uri;
-//import com.qualcomm.hardware.HiTechnicNxtTouchSensor;
+
 import com.qualcomm.hardware.HiTechnicNxtLightSensor;
 import com.qualcomm.hardware.HiTechnicNxtUltrasonicSensor;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
@@ -10,8 +10,14 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorController;
 import com.qualcomm.robotcore.hardware.Servo;
 import java.io.File;
+/*
+All imports and subroutines included within this folder for autonomous
+© Bullet Proof 6712
+All rights reserved to the Country of the USA
+ */
 public abstract class BulletOpMode extends LinearOpMode {
-    public HiTechnicNxtLightSensor lightSensor;
+    public HiTechnicNxtLightSensor leftLightSensor;
+    public HiTechnicNxtLightSensor rightLightSensor;
     public DcMotor leftfront;
     public DcMotor leftback;
     public DcMotor rightfront;
@@ -27,51 +33,32 @@ public abstract class BulletOpMode extends LinearOpMode {
     public DcMotorController leftController;
     public DcMotorController ArmController;
     public HiTechnicNxtUltrasonicSensor ultrasonicSensor;
-    //final double GEAR_ONE_TEETH = 16;
-    //final double GEAR_TWO_TEETH = 30;
     final double CLICKS_PER_REVOLUTION = 1120;
     final double WHEEL_CIRCUMFERENCE = 7 * Math.PI;
-    final double MOTOR_POWER = .5;
-    final double TURN_POWER = 0.5;
     final double GOAL_DIFFERENCE = 100;
     final double DISTANCE_FACTOR = 0.89;
     final double TURN_FACTOR = 2.22;
     final double RIGHT = 1;
     final double LEFT = -1;
-    final double MOTOR_POWER_LEFT = 0.9;
-    final double MOTOR_POWER_RIGHT = 0.65;
     final double ARM_GEAR_1 = 72;
     final double ARM_GEAR_2 = 20;
     final double ARM_GEAR_3 = 72;
     final double ARM_GEAR_4 = 34;
-    final double FRONT_WHEEL_RATIO = 1.8;
-    final double BACK_WHEEL_RATIO = 1;
     final double LEFT_FRONT_POWER = .165;
     final double RIGHT_FRONT_POWER = .155;
     final double LEFT_BACK_POWER = .15;
     final double RIGHT_BACK_POWER = .125;
-    final double ArmStop = 0;
-    final int armForward = 1;
-    final int armBack = -1;
-    final float ARM_POWER_FORWARD = (float) .2;
-    final float ARM_POWER_BACK = (float) -.2;
-    float PowerForward;
-    float PowerBack;
     final static double SERVO_SKIRT_UPR = .23;
     final static double SERVO_SKIRT_DOWNR= .1;
     final static double SERVO_SKIRT_UPL = .23;
     final static double SERVO_SKIRT_DOWNL= 0.1;
-    final static double LOWER_ARM_LEFT = .6;
-    final static double LOWER_ARM_RIGHT = .4;
-    final static double LOWER_ARM_OFF = .5;
-    final static double UPPER_ARM_LOCKED = .54;
     final static double UPPER_ARM_UNLOCKED = .25;
     final static double ZIPLINE_UP = .6;
-    final static double ZIPLINE_DOWN = .23;
     public static Context CONTEXT;
     public MediaPlayer Rocky;
-    //HiTechnicNxtTouchSensor ArmReset;
-    //float count  = 0;
+    public MediaPlayer Shia;
+    public MediaPlayer ShiaSurprise;
+    public MediaPlayer DoIt;
     protected final void Init() throws InterruptedException {
         ArmTiltLeft = hardwareMap.dcMotor.get("ArmTiltLeft");
         ArmTiltRight = hardwareMap.dcMotor.get("ArmTiltRight");
@@ -90,30 +77,36 @@ public abstract class BulletOpMode extends LinearOpMode {
         UpperArmLock = hardwareMap.servo.get("UpperArmLock");
         Zipline = hardwareMap.servo.get("Zipline");
         ultrasonicSensor = (HiTechnicNxtUltrasonicSensor) hardwareMap.ultrasonicSensor.get("SonarDrive");
-        lightSensor = (HiTechnicNxtLightSensor) hardwareMap.lightSensor.get("LightSensor");
-        //leftback.setDirection(DcMotor.Direction.REVERSE);
+        leftLightSensor = (HiTechnicNxtLightSensor) hardwareMap.lightSensor.get("LightSensor");
+        rightLightSensor = (HiTechnicNxtLightSensor) hardwareMap.lightSensor.get("RightLightSensor");
         rightfront.setDirection(DcMotor.Direction.REVERSE);
         ArmTiltLeft.setDirection(DcMotor.Direction.REVERSE);
         ArmController.setMotorControllerDeviceMode(DcMotorController.DeviceMode.WRITE_ONLY);
         waitCycle(6);
-        ArmTiltLeft.setChannelMode(DcMotorController.RunMode.RESET_ENCODERS);
+        ArmTiltLeft.setMode(DcMotorController.RunMode.RESET_ENCODERS);
         waitCycle(6);
-        ArmTiltLeft.setChannelMode(DcMotorController.RunMode.RUN_USING_ENCODERS);
+        ArmTiltLeft.setMode(DcMotorController.RunMode.RUN_USING_ENCODERS);
         waitCycle(6);
         leftController.setMotorControllerDeviceMode(DcMotorController.DeviceMode.WRITE_ONLY);
         waitCycle(6);
-        leftfront.setChannelMode(DcMotorController.RunMode.RESET_ENCODERS);
+        leftfront.setMode(DcMotorController.RunMode.RESET_ENCODERS);
         waitCycle(6);
-        leftfront.setChannelMode(DcMotorController.RunMode.RUN_USING_ENCODERS);
+        leftfront.setMode(DcMotorController.RunMode.RUN_USING_ENCODERS);
         waitCycle(6);
         SkirtServoR.setPosition(SERVO_SKIRT_UPR);
         SkirtServoL.setPosition(SERVO_SKIRT_UPL);
         UpperArmLock.setPosition(UPPER_ARM_UNLOCKED);
-        Zipline.setPosition(ZIPLINE_UP);//fix left front0
+        Zipline.setPosition(ZIPLINE_UP);
         Rocky = MediaPlayer.create(hardwareMap.appContext, Uri.fromFile(new File("/mnt/sdcard/rocky.mp3")));
         Rocky.setVolume(1, 1);
+        Shia = MediaPlayer.create(hardwareMap.appContext, Uri.fromFile(new File("/mnt/sdcard/Shia.mp3")));
+        Shia.setVolume(1, 1);
+        ShiaSurprise = MediaPlayer.create(hardwareMap.appContext, Uri.fromFile(new File("/mnt/sdcard/ShiaSurprise.mp3")));
+        ShiaSurprise.setVolume(1, 1);
+        DoIt = MediaPlayer.create(hardwareMap.appContext, Uri.fromFile(new File("/mnt/sdcard/DoIt.mp3")));
+        DoIt.setVolume(1, 1);
     }
-    public int GetTicks() throws InterruptedException { //gets the current value of the encoder
+    public int GetTicks() throws InterruptedException {
         leftController.setMotorControllerDeviceMode(DcMotorController.DeviceMode.READ_ONLY);
         waitCycle(6);
         int position = leftfront.getCurrentPosition();
@@ -121,35 +114,36 @@ public abstract class BulletOpMode extends LinearOpMode {
         waitCycle(6);
         return position;
     }
-    public void motorDrive(double LeftFrontPower, double RightFrontPower , double LeftBackPower , double RightBackPower){  //Driving forward
+    public void motorDrive(double LeftFrontPower, double RightFrontPower , double LeftBackPower , double RightBackPower){
         leftfront.setPower(LeftFrontPower);
         rightfront.setPower(RightFrontPower);
         leftback.setPower(LeftBackPower * -1);
         rightback.setPower(RightBackPower);
     }
-    public void StopDriveMotors(){  //Stopping the robot
+    public void StopDriveMotors(){
         leftfront.setPower(0);
         rightfront.setPower(0);
         leftback.setPower(0);
         rightback.setPower(0);
     }
-    public void StopArmMotors(){  //Stopping the robot
+    public void StopArmMotors(){
         ArmTiltLeft.setPower(0);
         ArmTiltRight.setPower(0);
     }
-    public void lightsensor(){
-            lightSensor.enableLed(true);
-            double LightSensorDataRaw = lightSensor.getLightDetectedRaw();
-                while (LightSensorDataRaw < 120) {
-                    LightSensorDataRaw = lightSensor.getLightDetectedRaw();
-                    telemetry.addData("LightSensorRaw" , LightSensorDataRaw);
-                    telemetry.addData("LightSensor", String.valueOf(lightSensor.getLightDetected()));
-                    motorDrive(LEFT_FRONT_POWER, RIGHT_FRONT_POWER, LEFT_BACK_POWER, RIGHT_BACK_POWER);
 
-                }
+    public void lightsensor(HiTechnicNxtLightSensor lightSensor) {
+        lightSensor.enableLed(true);
+        double LightSensorDataRaw = lightSensor.getLightDetectedRaw();
+
+        while (LightSensorDataRaw < 120) {
+            LightSensorDataRaw = lightSensor.getLightDetectedRaw();
+            motorDrive(LEFT_FRONT_POWER, RIGHT_FRONT_POWER, LEFT_BACK_POWER, RIGHT_BACK_POWER);
+        }
+
         StopDriveMotors();
     }
-    public void motorTurn(double LeftFrontPower , double RightFrontPower , double LeftBackPower , double RightBackPower , double direction){//Turning robot
+
+    public void motorTurn(double LeftFrontPower , double RightFrontPower , double LeftBackPower , double RightBackPower , double direction){
         double LeftFrontMotorPower =  direction * LeftFrontPower;
         double RightFrontMotorPower = -1 * direction * RightFrontPower;
         double LeftBackMotorPower = -1 * direction * LeftBackPower;
@@ -159,7 +153,7 @@ public abstract class BulletOpMode extends LinearOpMode {
         rightfront.setPower(RightFrontMotorPower);
         rightback.setPower(RightBackMotorPower);
     }
-    public void motorArmRotate(double Arm_power , double direction){//Turning robot
+    public void motorArmRotate(double Arm_power , double direction){
         double ArmFinalPower = direction * Arm_power;
         ArmTiltRight.setPower(ArmFinalPower);
         ArmTiltLeft.setPower(ArmFinalPower);
@@ -168,18 +162,18 @@ public abstract class BulletOpMode extends LinearOpMode {
         waitCycle(6);
         leftController.setMotorControllerDeviceMode(DcMotorController.DeviceMode.WRITE_ONLY);
         waitCycle(6);
-        leftfront.setChannelMode(DcMotorController.RunMode.RESET_ENCODERS);
+        leftfront.setMode(DcMotorController.RunMode.RESET_ENCODERS);
         waitCycle(6);
-        leftfront.setChannelMode(DcMotorController.RunMode.RUN_USING_ENCODERS);
+        leftfront.setMode(DcMotorController.RunMode.RUN_USING_ENCODERS);
         waitCycle(6);
     }
     public void ResetArmEncoders() throws InterruptedException {
         waitCycle(6);
         ArmController.setMotorControllerDeviceMode(DcMotorController.DeviceMode.WRITE_ONLY);
         waitCycle(6);
-        ArmTiltLeft.setChannelMode(DcMotorController.RunMode.RESET_ENCODERS);
+        ArmTiltLeft.setMode(DcMotorController.RunMode.RESET_ENCODERS);
         waitCycle(6);
-        ArmTiltLeft.setChannelMode(DcMotorController.RunMode.RUN_USING_ENCODERS);
+        ArmTiltLeft.setMode(DcMotorController.RunMode.RUN_USING_ENCODERS);
         waitCycle(6);
     }
     public void waitCycle(int count) throws InterruptedException {
@@ -202,29 +196,15 @@ public abstract class BulletOpMode extends LinearOpMode {
     public void ArmLiftStop (){
         ArmLift.setPower(0);
     }
-    //public void TouchSensor() throws InterruptedException{
-    //ArmReset = (HiTechnicNxtTouchSensor) hardwareMap.touchSensor.get("TouchSensor");
-    //String  touchValue;
 
-    //waitForStart();
+    public void initialize() {};
+    public void Update() {};
 
-    //while(opModeIsActive()){
-    //if (ArmReset.isPressed()) {
-    //touchValue = "Yes";
-    //ArmTiltLeft.setPower(1);//if motors aren't attached it won't work
-    //ArmTiltRight.setPower(1);
-    //}else {
-    // touchValue = "No";
-    //}
-    // telemetry.addData("Is it pressed?", touchValue);
-    //}
-    //}
-    public abstract void initialize();
-    public abstract void Update();
+    // TODO(): Can this handle a negative distance?
     public boolean goForward(int inches , double LeftFrontPower , double RightFrontPower , double LeftBackPower , double RightBackPower, long delay) throws InterruptedException {
-        double goal = (CLICKS_PER_REVOLUTION/WHEEL_CIRCUMFERENCE)*inches; //Get ticks for the distance needed to travel
+        double goal = (CLICKS_PER_REVOLUTION/WHEEL_CIRCUMFERENCE)*inches;
         goal = (goal - GOAL_DIFFERENCE) * DISTANCE_FACTOR;
-        ResetEncoders();//Puts program in read only inside the routine
+        ResetEncoders();
         waitCycle(6);
         int ticks ;
         leftController.setMotorControllerDeviceMode(DcMotorController.DeviceMode.WRITE_ONLY);
@@ -252,7 +232,7 @@ public abstract class BulletOpMode extends LinearOpMode {
     }
     public void goTurn(int degrees, double direction, double LeftFrontPower , double RightFrontPower , double LeftBackPower , double RightBackPower) throws InterruptedException {
         int ticks ;
-        double goal = (CLICKS_PER_REVOLUTION)*degrees/360;//Get ticks for the distance needed to travel
+        double goal = (CLICKS_PER_REVOLUTION)*degrees/360;
         goal = goal * TURN_FACTOR;
         ResetEncoders();
         waitCycle(6);
@@ -272,7 +252,7 @@ public abstract class BulletOpMode extends LinearOpMode {
     }
     public void armRotate(int degrees, int direction, double arm_power) throws InterruptedException {
         int ticks ;
-        double goal = ((ARM_GEAR_1/ARM_GEAR_2)*(ARM_GEAR_3/ARM_GEAR_4))*(CLICKS_PER_REVOLUTION)*degrees/360;//Get ticks for the distance needed to travel
+        double goal = ((ARM_GEAR_1/ARM_GEAR_2)*(ARM_GEAR_3/ARM_GEAR_4))*(CLICKS_PER_REVOLUTION)*degrees/360;
         ResetArmEncoders();
         waitCycle(6);
         ArmController.setMotorControllerDeviceMode(DcMotorController.DeviceMode.WRITE_ONLY);
