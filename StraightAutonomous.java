48a02f5babd3ca3764e2286eaeac75fb766c1350
NewbergFTC <RevolutionNewbergFTC@gmail.com>
2016-02-04 16:55:26 -0800

    Update and rename StraightAutonomous to StraightAutonomous.java

--- StraightAutonomous.java
+++ StraightAutonomous.java
@@ -1,16 +1,17 @@
-package com.qualcomm.ftcrobotcontroller.opmodes;
-
-import com.qualcomm.ftcrobotcontroller.opmodes.BulletOpMode;
-
+package com.qualcomm.ftcrobotcontroller.us.newberg.bullet;
+/*
+Autonomous program to move robot into loading zone
+in a straight line
+© Bullet Proof 6712
+All rights reserved to the Country of the USA
+ */
 public class StraightAutonomous extends BulletOpMode {
     @Override
     public void initialize() {
     }
-
     @Override
     public void Update() {
     }
-
     @Override
     public void runOpMode() throws InterruptedException {
         super.Init();
@@ -19,19 +20,14 @@ public class StraightAutonomous extends BulletOpMode {
             Rocky.start();
             SkirtServoR.setPosition(SERVO_SKIRT_DOWNR);
             SkirtServoL.setPosition(SERVO_SKIRT_DOWNL);
-            //if (goForward(5, LEFT_FRONT_POWER, RIGHT_FRONT_POWER, LEFT_BACK_POWER, RIGHT_BACK_POWER, 1000))
-            //    return;
             sleep(5000);
             double ultrasonicvalue = ultrasonicSensor.getUltrasonicLevel();
-            while (ultrasonicvalue > 35){
+            while (ultrasonicvalue > 35){ // The Ultrasonic value is equal to 35 centimeters
                 ultrasonicvalue = ultrasonicSensor.getUltrasonicLevel();
                 motorDrive(LEFT_FRONT_POWER,RIGHT_FRONT_POWER,LEFT_BACK_POWER,RIGHT_BACK_POWER);
             }
             StopDriveMotors();
-            //if (goForward(90, LEFT_FRONT_POWER, RIGHT_FRONT_POWER, LEFT_BACK_POWER, RIGHT_BACK_POWER,12000))
-                //return;
-
-            //break;
+            break;
         }
     }
 }
