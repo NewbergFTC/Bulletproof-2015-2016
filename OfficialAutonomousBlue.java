7443ab7556871fe01b61b0992922a2af93b150a7
NewbergFTC <RevolutionNewbergFTC@gmail.com>
2016-02-04 16:55:04 -0800

    Update OfficialAutonomousBlue.java

--- OfficialAutonomousBlue.java
+++ OfficialAutonomousBlue.java
@@ -1,7 +1,9 @@
-package com.qualcomm.ftcrobotcontroller.opmodes;
-
-        import com.qualcomm.ftcrobotcontroller.opmodes.BulletOpMode;
-
+package com.qualcomm.ftcrobotcontroller.us.newberg.bullet;
+/*
+Autonomous program to move robot into loading zone
+© Bullet Proof 6712
+All rights reserved to the Country of the USA
+ */
 public class OfficialAutonomousBlue extends BulletOpMode {
     @Override
     public void initialize() {
@@ -14,42 +16,22 @@ public class OfficialAutonomousBlue extends BulletOpMode {
         super.Init();
         waitForStart();
         while (opModeIsActive()) {
-            SkirtServoR.setPosition(SERVO_SKIRT_DOWNR);
-            SkirtServoL.setPosition(SERVO_SKIRT_DOWNL);
-            Rocky.start();
-            //if (goForward(5 , LEFT_FRONT_POWER,RIGHT_FRONT_POWER,LEFT_BACK_POWER ,RIGHT_BACK_POWER,3500 ))
-            //    return;
-            sleep(10000);
-            if (goForward(42 , LEFT_FRONT_POWER,RIGHT_FRONT_POWER,LEFT_BACK_POWER ,RIGHT_BACK_POWER,3500 ))
+            if (goForward(5 , LEFT_FRONT_POWER,RIGHT_FRONT_POWER,LEFT_BACK_POWER ,RIGHT_BACK_POWER,3500 ))
+                return;
+            sleep(9000);
+            if (goForward(37 , LEFT_FRONT_POWER,RIGHT_FRONT_POWER,LEFT_BACK_POWER ,RIGHT_BACK_POWER,3500 ))
                 return;
             sleep(500);
             goTurn(64, RIGHT, LEFT_FRONT_POWER,RIGHT_FRONT_POWER,LEFT_BACK_POWER,RIGHT_BACK_POWER);
-            // goForward(20, MOTOR_POWER);
             sleep(500);
             if (goForward(42 , LEFT_FRONT_POWER , RIGHT_FRONT_POWER,LEFT_BACK_POWER,RIGHT_BACK_POWER,3500))
                 return;
             sleep(500);
-            goTurn(58, RIGHT, LEFT_FRONT_POWER, RIGHT_FRONT_POWER, LEFT_BACK_POWER, RIGHT_BACK_POWER);
+            goTurn(58, RIGHT, LEFT_FRONT_POWER,RIGHT_FRONT_POWER,LEFT_BACK_POWER,RIGHT_BACK_POWER);
             sleep(500);
-            double ultrasonicvalue = ultrasonicSensor.getUltrasonicLevel();
-            while (ultrasonicvalue > 22){
-                ultrasonicvalue = ultrasonicSensor.getUltrasonicLevel();
-                motorDrive(LEFT_FRONT_POWER,RIGHT_FRONT_POWER,LEFT_BACK_POWER,RIGHT_BACK_POWER);
-            }
-            StopDriveMotors();
-            //if (goForward(45 , LEFT_FRONT_POWER , RIGHT_FRONT_POWER, LEFT_BACK_POWER , RIGHT_BACK_POWER,3500))
-            //    return;//accelerate into wall
+            if (goForward(45 , LEFT_FRONT_POWER , RIGHT_FRONT_POWER, LEFT_BACK_POWER , RIGHT_BACK_POWER,3500))
+                return;
             sleep(500);
-            motorArmRotate(.12,1);
-            sleep(5500);
-            StopArmMotors();
-            sleep(2000);
-            ArmLiftUp();
-            sleep(500);
-            ArmLiftStop();
-            sleep(500);
-            //armRotate(100 , armForward, PowerForward );
-            //sleep(500);
             break;
 
         }
