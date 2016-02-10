de9ace4b9ed696d41ddeb85a13844acf4055f885
NewbergFTC <RevolutionNewbergFTC@gmail.com>
2016-02-10 15:54:06 -0800

    Update BlueLightSensorAutonomous.java

--- BlueLightSensorAutonomous.java
+++ BlueLightSensorAutonomous.java
@@ -1,51 +1,44 @@
 package com.qualcomm.ftcrobotcontroller.us.newberg.bullet;
 
-/**
- * TODO(): A non-bs header
- *
- * Autonomous opmode that scores the climbers in the blue bucket, and lands us in the parking zone
- * Uses an ultrasonic and color sensor
+/*
+Autonomous utilizing the light sensor and the ultrasonic sensor
+to deposit climbers in the bucket
+© Bullet Proof 6712
+All rights reserved to the Country of the USA
  */
 public class BlueLightSensorAutonomous extends BulletOpMode {
-
     @Override
     public void runOpMode() throws InterruptedException {
         super.Init();
         waitForStart();
-
         if (!opModeIsActive())
             return;
-
         // TODO(): Update this for the new skirt system
-        SkirtServoR.setPosition(SERVO_SKIRT_DOWNR);
+        SkirtServoR.setPosition(SERVO_SKIRT_DOWN);
         SkirtServoL.setPosition(SERVO_SKIRT_DOWNL);
-
         Rocky.start();
         //Shia.start();
-
-        lightsensor(rightLightSensor);
-        sleep(500);
-
-//        if (goForward(5, LEFT_FRONT_POWER * 1.2, RIGHT_FRONT_POWER, LEFT_BACK_POWER * 1.2, RIGHT_BACK_POWER, 5000))
-//            return;
+        lightSensor(rightLightSensor);
+        sleep(1000);
+        /*
+        if (goForward(5, LEFT_FRONT_POWER * 1.2, RIGHT_FRONT_POWER, LEFT_BACK_POWER * 1.2, RIGHT_BACK_POWER, 5000))
+            return;
         motorDrive(LEFT_FRONT_POWER, RIGHT_FRONT_POWER, LEFT_BACK_POWER, RIGHT_BACK_POWER);
         sleep(750);
         StopDriveMotors();
 
-        lightsensor(rightLightSensor);
+        lightSensor(rightLightSensor);
         sleep(500);
-
-        motorDrive(-LEFT_FRONT_POWER,-RIGHT_FRONT_POWER,-LEFT_BACK_POWER,-RIGHT_BACK_POWER);
+        */
+        motorDrive(-LEFT_FRONT_POWER, -RIGHT_FRONT_POWER, -LEFT_BACK_POWER, -RIGHT_BACK_POWER);
         sleep(1000);
         StopDriveMotors();
-
-        goTurn(160, RIGHT, LEFT_FRONT_POWER * 1.65, RIGHT_FRONT_POWER * 1.2, LEFT_BACK_POWER * 1.5, RIGHT_BACK_POWER * 1.35);
         sleep(500);
-
+        goTurn(155, RIGHT, LEFT_FRONT_POWER * 1.65, RIGHT_FRONT_POWER * 1.2, LEFT_BACK_POWER * 1.5, RIGHT_BACK_POWER * 1.35);
+        sleep(500);
         while (ultrasonicSensor.getUltrasonicLevel() > 22) {
             motorDrive(LEFT_FRONT_POWER, RIGHT_FRONT_POWER, LEFT_BACK_POWER, RIGHT_BACK_POWER);
         }
-
         StopDriveMotors();
         Rocky.pause();
         sleep(200);
@@ -74,7 +67,6 @@ public class BlueLightSensorAutonomous extends BulletOpMode {
         StopArmMotors();
         sleep(500);
 
-        // TODO(): Is this really needed?
         motorDrive(-LEFT_FRONT_POWER, -RIGHT_FRONT_POWER, -LEFT_BACK_POWER, -RIGHT_BACK_POWER);
         sleep(550);
 
