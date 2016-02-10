e62b470712efaa5a5036cacaa16bbc0d96a9c89e
NewbergFTC <RevolutionNewbergFTC@gmail.com>
2016-02-10 15:53:15 -0800

    Update OfficialTeleop.java

--- OfficialTeleop.java
+++ OfficialTeleop.java
@@ -1,11 +1,20 @@
 package com.qualcomm.ftcrobotcontroller.us.newberg.bullet;
+
 import android.media.MediaPlayer;
 import android.net.Uri;
+
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
+
 import java.io.File;
-public class OfficialTeleOp extends OpMode{
+
+/*
+Main Tele-Op Program used in all competitions
+© Bullet Proof 6712
+All rights reserved to the Country of the USA
+*/
+public class OfficialTeleOp extends OpMode {
     private DcMotor leftfront;
     private DcMotor leftback;
     private DcMotor rightfront;
@@ -13,31 +22,31 @@ public class OfficialTeleOp extends OpMode{
     private DcMotor ArmTiltLeft;
     private DcMotor ArmTiltRight;
     private DcMotor ArmLift;
-    private Servo SkirtServoR;
-    private Servo SkirtServoL;
+    private Servo SkirtServo;
     private Servo LowerArmLock;
     private Servo UpperArmLock;
-    private Servo Zipline;
     float PowerForward = (float) .1;
     float PowerBack = (float) -.1;
     final static float LIFT_POWER_FORWARD = (float) 1;
     final static float LIFT_POWER_BACK = -1;
-    final static double SERVO_SKIRT_UPR = .23;
-    final static double SERVO_SKIRT_DOWNR= .1;
-    final static double SERVO_SKIRT_UPL = .23;
-    final static double SERVO_SKIRT_DOWNL= 0.11;
+    final static double SERVO_SKIRT_UP = .215;
+    final static double SERVO_SKIRT_DOWN = .11;
+    //final static double SERVO_SKIRT_UPL = .21;
+    //final static double SERVO_SKIRT_DOWNL= 0.11;
     final static double LOWER_ARM_LEFT = .6;
     final static double LOWER_ARM_RIGHT = .4;
     final static double LOWER_ARM_OFF = .5;
-    final static double UPPER_ARM_LOCKED = .9;
+    final static double UPPER_ARM_LOCKED = 1;
     final static double UPPER_ARM_UNLOCKED = .0;
-    final static double ZIPLINE_UP = .6;
-    final static double ZIPLINE_DOWN = .23 ;
     public MediaPlayer Rocky;
     public MediaPlayer ShiaSurprise;
     public MediaPlayer DoIt;
+    public MediaPlayer poundCake;
+    public MediaPlayer vamos;
+    public MediaPlayer roboto;
+
     @Override
-    public void init(){
+    public void init() {
         leftfront = hardwareMap.dcMotor.get("lf");
         leftback = hardwareMap.dcMotor.get("lb");
         rightfront = hardwareMap.dcMotor.get("rf");
@@ -48,14 +57,12 @@ public class OfficialTeleOp extends OpMode{
         ArmLift = hardwareMap.dcMotor.get("ArmLift");
         ArmLift.setDirection(DcMotor.Direction.REVERSE);
         ArmTiltLeft.setDirection(DcMotor.Direction.REVERSE);
-        SkirtServoR = hardwareMap.servo.get("SkirtservoR");
-        SkirtServoL = hardwareMap.servo.get("SkirtservoL");
+        SkirtServo = hardwareMap.servo.get("SkirtservoR");
         LowerArmLock = hardwareMap.servo.get("LowerArmLock");
         LowerArmLock.setDirection(Servo.Direction.REVERSE);
         UpperArmLock = hardwareMap.servo.get("UpperArmLock");
-        Zipline = hardwareMap.servo.get("Zipline");
-        SkirtServoR.setPosition(SERVO_SKIRT_DOWNR);
-        SkirtServoL.setPosition(SERVO_SKIRT_DOWNL);
+        SkirtServo.setPosition(SERVO_SKIRT_DOWN);
+        //SkirtServoL.setPosition(SERVO_SKIRT_DOWNL);
         UpperArmLock.setPosition(UPPER_ARM_UNLOCKED);
         LowerArmLock.setPosition(LOWER_ARM_OFF);
         Rocky = MediaPlayer.create(hardwareMap.appContext, Uri.fromFile(new File("/mnt/sdcard/rocky.mp3")));
@@ -64,22 +71,42 @@ public class OfficialTeleOp extends OpMode{
         ShiaSurprise.setVolume(1, 1);
         DoIt = MediaPlayer.create(hardwareMap.appContext, Uri.fromFile(new File("/mnt/sdcard/DoIt.mp3")));
         DoIt.setVolume(1, 1);
+        poundCake = MediaPlayer.create(hardwareMap.appContext, Uri.fromFile(new File("/mnt/sdcard/PoundCake.mp3")));
+        poundCake.setVolume(1, 1);
+        vamos = MediaPlayer.create(hardwareMap.appContext, Uri.fromFile(new File("/mnt/sdcard/Vamos.mp3")));
+        vamos.setVolume(1, 1);
+        roboto = MediaPlayer.create(hardwareMap.appContext, Uri.fromFile(new File("/mnt/sdcard/Roboto.mp3")));
+        roboto.setVolume(1, 1);
+
+        DoIt.start();
+        poundCake.start();
+        vamos.start();
+        roboto.start();
+        Rocky.start();
+        ShiaSurprise.start();
+        DoIt.pause();
+        poundCake.pause();
+        vamos.pause();
+        roboto.pause();
+        Rocky.pause();
+        ShiaSurprise.pause();
     }
+
     @Override
-    public void loop(){
+    public void loop() {
         float leftY = gamepad1.left_stick_y;
         float rightY = -gamepad1.right_stick_y;
-        double backleftpower = (leftY ) * .75;
-        double backrightpower = (rightY ) * .75;
+        double backleftpower = (leftY) * .75;
+        double backrightpower = (rightY) * .75;
         double frontleftpower = (leftY * .9 * .75 * -1);
-        double frontrightpower = (rightY * .9)* .75;
+        double frontrightpower = (rightY * .9) * .75;
         leftfront.setPower(frontleftpower);
         rightfront.setPower(frontrightpower);
         leftback.setPower(backleftpower);
         rightback.setPower(backrightpower);
 
-        PowerForward = (gamepad2.a)? -1 : (float) -0.2;
-        PowerBack = (gamepad2.a)? 1 : (float) 0.2;
+        PowerForward = (gamepad2.a) ? -1 : (float) -0.2;
+        PowerBack = (gamepad2.a) ? 1 : (float) 0.2;
 
         float armPower = (gamepad2.left_trigger >= 0.05) ? PowerForward : (gamepad2.right_trigger >= 0.05) ? PowerBack : 0;
         ArmTiltLeft.setPower(armPower);
@@ -88,46 +115,97 @@ public class OfficialTeleOp extends OpMode{
         float extendPower = (gamepad2.left_bumper) ? LIFT_POWER_FORWARD : (gamepad2.right_bumper) ? LIFT_POWER_BACK : 0;
         ArmLift.setPower(extendPower);
 
-        if (gamepad1.dpad_up){
-            SkirtServoR.setPosition(SERVO_SKIRT_UPR);
-            SkirtServoL.setPosition(SERVO_SKIRT_UPL);
+        if (gamepad1.dpad_up) {
+            SkirtServo.setPosition(SERVO_SKIRT_UP);
+            //SkirtServoL.setPosition(SERVO_SKIRT_UPL);
 
         }
-        if (gamepad1.dpad_down){
-            SkirtServoR.setPosition(SERVO_SKIRT_DOWNR);
-            SkirtServoL.setPosition(SERVO_SKIRT_DOWNL);
+        if (gamepad1.dpad_down) {
+            SkirtServo.setPosition(SERVO_SKIRT_DOWN);
+            //SkirtServoL.setPosition(SERVO_SKIRT_DOWNL);
         }
 
-        LowerArmLock.setPosition( gamepad1.a ? LOWER_ARM_RIGHT :
+        LowerArmLock.setPosition(gamepad1.a ? LOWER_ARM_RIGHT :
                 gamepad1.b ? LOWER_ARM_LEFT : LOWER_ARM_OFF);
 
-        if(gamepad1.x){
+        if (gamepad1.x) {
             UpperArmLock.setPosition(UPPER_ARM_LOCKED);
         }
-        if(gamepad1.y){
+        if (gamepad1.y) {
             UpperArmLock.setPosition(UPPER_ARM_UNLOCKED);
         }
 
-        if(gamepad2.x){
-            Zipline.setPosition(ZIPLINE_DOWN);
-        }
-        if(gamepad2.y){
-            Zipline.setPosition(ZIPLINE_UP);
-        }
-        if(gamepad2.dpad_left){
-            DoIt.stop();
-            ShiaSurprise.stop();
+
+        /*
+        Epic Sound Board
+        To Intimidate All Other Teams
+        */
+
+        // Rocky
+        if (gamepad2.dpad_left) {
+            ShiaSurprise.pause();
+            DoIt.pause();
+            poundCake.pause();
+            vamos.pause();
+            roboto.pause();
+
+            Rocky.seekTo(0);
             Rocky.start();
         }
-        if(gamepad2.dpad_up){
-            DoIt.stop();
-            Rocky.stop();
+
+        if (gamepad2.dpad_up) {
+            DoIt.pause();
+            poundCake.pause();
+            vamos.pause();
+            roboto.pause();
+            Rocky.pause();
+
+            ShiaSurprise.seekTo(0);
             ShiaSurprise.start();
         }
-        if(gamepad2.dpad_right){
-            ShiaSurprise.stop();
-            Rocky.stop();
+
+        if (gamepad2.dpad_right) {
+            poundCake.pause();
+            vamos.pause();
+            roboto.pause();
+            Rocky.pause();
+            ShiaSurprise.pause();
+
+            DoIt.seekTo(0);
             DoIt.start();
+        }
+
+        if (gamepad2.dpad_down) {
+            Rocky.pause();
+            ShiaSurprise.pause();
+            DoIt.pause();
+            vamos.pause();
+            roboto.pause();
+
+            poundCake.seekTo(0);
+            poundCake.start();
+        }
+
+        if (gamepad2.right_stick_button) {
+            Rocky.pause();
+            ShiaSurprise.pause();
+            DoIt.pause();
+            poundCake.pause();
+            roboto.pause();
+
+            vamos.seekTo(0);
+            vamos.start();
+        }
+
+        if (gamepad2.left_stick_button) {
+            Rocky.pause();
+            ShiaSurprise.pause();
+            DoIt.pause();
+            poundCake.pause();
+            vamos.pause();
+
+            roboto.seekTo(0);
+            roboto.start();
         }
     }
 }
